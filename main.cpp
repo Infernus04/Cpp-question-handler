84c557281535a8c1edbe553b80f60d8eaa063e66
Ishan <magardeishan@gmail.com>
2024-07-17 23:40:20 +0530

    sorting of array using bubble sort

--- main.cpp
+++ main.cpp
@@ -1,49 +1,49 @@
-//Below is an example of C++ code that sort the array by selection sort
+//Below is an example of C++ code that sort the array by Bubble sort
 
 #include<iostream>
 
 using namespace std;
 
 int main(){
+
+    int n;
+    cout<<"Enter the number of element in your array"<<endl;
+    cin>>n;
     
-    int n ;
-    cout<<"Enter the number of elements in your array"<<endl;
-    cin >> n;
-
-    cout<<"Enter the numbers"<<endl;
-
     int arr[n];
     for (int i = 0; i < n; i++)
     {
         cin>>arr[i];
     }
-    
 
-    for (int i = 0; i < n-1; i++)
+    int counter = 1 ;
+    while (counter<n)
     {
-        for (int j = i+1; j< n; j++)
+        for (int i = 0; i < n-counter; i++)
         {
-            if (arr[j] < arr[i])
+            if (arr[i] > arr[i+1])
             {
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
             }
             
         }
+        counter++;
         
     }
 
-    cout<<"Sorted array is : "<<endl;
+    cout<<"Sorted array is :"<<endl;
+
     for (int i = 0; i < n; i++)
     {
-        cout<<" "<<arr[i];
+        cout<<arr[i]<<" ";
+
     }
     
     
-        
     
-
+    
     
     return 0;
 }
