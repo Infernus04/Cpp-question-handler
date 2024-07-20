97469d8c7e068b3a0cc35c9e0debd9224ca00732
Ishan <magardeishan@gmail.com>
2024-07-20 17:36:04 +0530

    pascal triangle pattern

--- main.cpp
+++ main.cpp
@@ -1,29 +1,40 @@
-// Below is an example of C++ code that prints the Floyd's triangle
+// Below is an example of C++ code that prints the pascal triangle
+
+//1
+//1 1
+//1 2 1
+//1 3 3 1 
+//1 4 6 4 1
 
 #include <iostream>
 
 using namespace std;
 
+int fact(int n){
+   int factorial = 1;
+   for (int i = 2; i <= n; i++)
+   {
+      factorial *= i;
+   }
+   return factorial;
+   
+}
+
+
 int main()
 {
   int n;
-  cout<<"Enter the number of rows : "<<endl;
+  cout<<"Enter the number of rows"<<endl;
   cin>>n;
 
-  cout<<"Floyd's triangle for given number of rows is"<<endl;
-
-  int count = 1;
-  
-  for (int i = 1; i <= n; i++)
+  for (int i = 0; i < n; i++)
   {
-    for (int j = 1; j <= i; j++)
-    {
-      cout<<count<<" ";
-      count++;
-    }
-
-    cout<<endl;
-    
+   for (int j = 0; j <= i; j++)
+   {
+      cout<<fact(i)/(fact(j)*fact(i-j))<<" ";
+   }
+   cout<<endl;
+   
   }
   
   
