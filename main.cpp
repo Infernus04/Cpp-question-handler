280a83a83f26dc8b663589f34508002e2a57442c
Ishan <magardeishan@gmail.com>
2024-07-20 18:49:45 +0530

    sum of subarray of an array

--- main.cpp
+++ main.cpp
@@ -1,42 +1,35 @@
-// Below is an example of C++ code that prints the pascal triangle
-
-//1
-//1 1
-//1 2 1
-//1 3 3 1 
-//1 4 6 4 1
+// Below is an example of C++ code that prints the sum of subarray of the given array
 
 #include <iostream>
 
 using namespace std;
 
-int fact(int n){
-   int factorial = 1;
-   for (int i = 2; i <= n; i++)
+int main(){
+
+   int n;
+   cout<<"Enter the number of elements for an array"<<endl;
+   cin >> n;
+
+   cout<<"Enter the elements :"<<endl;
+
+   int arr[n];
+   for (int i = 0; i < n; i++)
    {
-      factorial *= i;
+      cin >> arr[i];
    }
-   return factorial;
    
-}
-
-
-int main()
-{
-  int n;
-  cout<<"Enter the number of rows"<<endl;
-  cin>>n;
-
-  for (int i = 0; i < n; i++)
-  {
-   for (int j = 0; j <= i; j++)
+   int sum = 0;
+   for (int i = 0; i < n; i++)
    {
-      cout<<fact(i)/(fact(j)*fact(i-j))<<" ";
-   }
-   cout<<endl;
+      sum = 0;
+      for (int j = i; j < n ; j++)
+      {
+         sum += arr[j];
+         cout<<sum<<endl;
+      };
+      
+   };
    
-  }
-  
   
   
    return 0;
