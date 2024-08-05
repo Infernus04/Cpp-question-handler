8864d4bbd5c99da7604318de3d59c280686fdf93
Ishan <magardeishan@gmail.com>
2024-08-05 18:00:51 +0530

    Printing all the subarrays of an array

--- main.cpp
+++ main.cpp
@@ -1,38 +1,31 @@
+//Printing all the subarray of an array
+
 #include <bits/stdc++.h>
 using namespace std;
 
-//get BIT 
-int getBit(int num , int pos){
-    return ((num & (1<<pos)) != 0 );
-}
-
-//set BIT
-int setBit(int num, int pos){
-    return (num | (1<<pos)); 
-}
-
-//clear BIT (Takin compliment of original bit then applying AND operator)
-int clearBit(int num , int pos){
-    int mask = ~(1<<pos);
-    return (num & mask);
-}
-
-//update BIT (clear the bit at the given position then set the bit to the desired value)
-int updateBit(int num , int pos ,int value){
-    int mask = ~(1<<pos);
-    num = num & mask;
-    return (num | (value<<pos));
-
-}
-
 int main(){
-    int num,pos;
-    cout<<getBit(5, 2)<<endl;
+    int n;
+    cin>>n;
 
-    cout<<setBit(5,1)<<endl;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
 
-    cout<<clearBit(5,2)<<endl;
-
-    cout<<updateBit(5,1,1)<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                cout<<arr[k]<<" ";
+            }cout<<endl;
+            
+        }
+        
+    }
+    
+    
     return 0;
 }
